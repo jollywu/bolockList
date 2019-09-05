fb869efad3b7c856cbd265b54410d6af34d95614
吴乾亮 <jolly.wu@zkteco.com>
2019-09-05 14:57:33 +0800

    初始化工程

---
+++ person/README.md
@@ -0,0 +1,13 @@
+# @umi-material/person
+
+person
+
+## Usage
+
+```sh
+umi block https://github.com//tree/master/person
+```
+
+## LICENSE
+
+MIT
---
+++ person/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.css';
+
+const PAGE_NAME_UPPER_CAMEL_CASE: React.FC = () => {
+  return (
+    <div className={styles.normal}>
+      <h1>I am a umi block!</h1>
+    </div>
+  );
+}
+
+export default PAGE_NAME_UPPER_CAMEL_CASE;
